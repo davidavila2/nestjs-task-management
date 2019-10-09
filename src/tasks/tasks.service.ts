5ba29d426a02197bb8c39109e90ca91cbb9e0992
david <david@venturplex.com>
2019-10-09 07:37:55 -0700

    updated error

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './tasks.model';
 import * as uuid from 'uuid/v1';
 import { CreateTaskDto } from './dto/create-task-dto';
@@ -35,7 +35,13 @@ export class TasksService {
 
   // this method allows the controller to get a specific task by ID
   getTaskById(id: string): Task {
-    return this.tasks.find(task => task.id === id);
+    const found = this.tasks.find(task => task.id === id);
+
+    if (!found) {
+      throw new NotFoundException(`Task with ID '${id}' not found`);
+    }
+
+    return found;
   }
 
   // this method allows the controller to create a new task
@@ -52,7 +58,6 @@ export class TasksService {
     this.tasks.push(task);
     return task;
   }
-
 
   // this allows the controller to get a specific task by id and update a task accordingly
   updateTaskStatus(id: string, status: TaskStatus): Task {
